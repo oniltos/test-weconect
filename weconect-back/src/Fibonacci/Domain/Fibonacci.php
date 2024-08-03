ee5e187668999ef43efbe9b3ec3759e14228dc16
Nilton Freitas <hello@niltonfreitas.com>
2024-08-02 23:22:40 -0300

    Optimized fibonacci

--- weconect-back/src/Fibonacci/Domain/Fibonacci.php
+++ weconect-back/src/Fibonacci/Domain/Fibonacci.php
@@ -5,19 +5,37 @@ declare(strict_types=1);
 namespace Fibonacci\Domain;
 
 class Fibonacci {
-    public function generate(int $n): array {
-        $sequence = [];
+    /**
+     * Optimized version of fibonnaci generator function with time and space complexity O(n)
+     * and memory efficiency by storing only the last two computed values when needed
+     * 
+     * @param mixed $n
+     * @return int[]
+     */
+    public function generate($n)
+    {
+        if ($n <= 0) {
+            return [];
+        }
 
-        for ($i = 0; $i < $n; $i++) {
-            if ($i === 0) {
-                $sequence[] = 0;
-            } elseif ($i === 1) {
-                $sequence[] = 1;
-            } else {
-                $sequence[] = $sequence[$i - 1] + $sequence[$i - 2];
-            }
+        if ($n == 1) {
+            return [0];
+        }
+
+        $sequence = [0, 1];
+
+        $a = 0;
+        $b = 1;
+
+        for ($i = 2; $i < $n; $i++) {
+            $next = $a + $b;
+            $sequence[] = $next;
+            $a = $b;
+            $b = $next;
         }
 
         return $sequence;
     }
+
+    
 }
